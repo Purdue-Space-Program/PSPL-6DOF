2e8ee8265b658384058b0c66c8e8403c16140598
prest <preston.wright0330@gmail.com>
2025-08-30 11:42:56 -0400

    Initial para changes

--- TheSixDoF/MainRK4.m
+++ TheSixDoF/MainRK4.m
@@ -51,10 +51,10 @@ env = Env.Environment;
 
 % Set the end condition, timestep, simulation fidelity, and outputs
 % run 'help Sim.IntegratorSettings' for more details
-sim = Sim.IntegratorSettings('burnout', 0.1, 'medium');
+sim = Sim.IntegratorSettings('full', 0.1, 'medium');
 
 % set the outputs to be shown:
-output = 0;
+output = 1;
 
 % run rotation visualization (outputs must be on also)
 rotationVis = 'on';
