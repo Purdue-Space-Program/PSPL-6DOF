a84e7ed1bd9bfa0893c04785f8827c1031faff7d
Caleb Rice <caleb412011@icloud.com>
2025-09-18 17:41:02 -0400

    revamped rocket class (WIP)
    not yet operational, just syncing between my laptop and desktop

--- TheSixDoF/+Rocket/Rocket.m
+++ TheSixDoF/+Rocket/Rocket.m
@@ -3,14 +3,6 @@ classdef Rocket
     % properties of the rocket itself. If no inputs are given, the
     % values default to values for CMS.
     %
-    % INPUTS:
-    % name = the name of the rocket system
-    % refArea = reference area for aerodynamics [m^2]
-    % thrust = nominal sea level thrust [N]
-    % exitArea = exit area of nozzle [m^2]
-    % exitPressure = exit pressure [Pa]
-    % radius = radius of rocket [m]
-    % length = total length of rocket [m]
     %
     % OUTPUTS:
 
@@ -26,17 +18,14 @@ classdef Rocket
 
     methods
 
-        function rocket = CreateRocket(rocket,name,refArea,thrust,exitArea,exitPressure,radius,length)
-            arguments
-                rocket Rocket
-                name (1,1) string = "CMS"
-                refArea (1,1) double = 0.02224
-                thrust (1,1) double = 4270.29
-                exitArea (1,1) = 0.0070573
-                exitPressure (1,1) = 75842.3
-                radius (1,1) = 0.0841375
-                length (1,1) = 5.123688
-            end
+        function rocket = rocket()
+
+            name_prompt = "Input the name of a saved rocket (listed below), '1' to save a new rocket, or '2' to create a temporary object (not saved). ";
+            name_struct = dir("TheSixDoF\Inputs\Saved Saved Rockets\*.mat");
+            rocket_num = length(name_struct);
+            
+            input(name_prompt+names,"s")
+
 
             rocket.name = name;
             rocket.refArea = refArea;
@@ -45,6 +34,7 @@ classdef Rocket
             rocket.exitPressure = exitPressure;
             rocket.radius = radius;
             rocket.length = length;
+            rocket.drag = 
         end
 
         function drawRocket(rocket)
