d4e9909957f0f916c1b3296986e1db20d39e68ee
HudsonReynolds <73318449+HudsonReynolds@users.noreply.github.com>
2025-09-20 17:34:11 -0400

    create rocket gui

---
+++ TheSixDoF/RocketGUI_exported.m
@@ -0,0 +1,130 @@
+classdef RocketGUI_exported < matlab.apps.AppBase
+
+    % Properties that correspond to app components
+    properties (Access = public)
+        UIFigure                    matlab.ui.Figure
+        GridLayout                  matlab.ui.container.GridLayout
+        LeftPanel                   matlab.ui.container.Panel
+        GridLayout2                 matlab.ui.container.GridLayout
+        ComponentSelectionDropDown  matlab.ui.control.DropDown
+        ComponentSelectionDropDownLabel  matlab.ui.control.Label
+        RightPanel                  matlab.ui.container.Panel
+        UIAxes                      matlab.ui.control.UIAxes
+    end
+
+    % Properties that correspond to apps with auto-reflow
+    properties (Access = private)
+        onePanelWidth = 576;
+    end
+
+    % Callbacks that handle component events
+    methods (Access = private)
+
+        % Changes arrangement of the app based on UIFigure width
+        function updateAppLayout(app, event)
+            currentFigureWidth = app.UIFigure.Position(3);
+            if(currentFigureWidth <= app.onePanelWidth)
+                % Change to a 2x1 grid
+                app.GridLayout.RowHeight = {480, 480};
+                app.GridLayout.ColumnWidth = {'1x'};
+                app.RightPanel.Layout.Row = 2;
+                app.RightPanel.Layout.Column = 1;
+            else
+                % Change to a 1x2 grid
+                app.GridLayout.RowHeight = {'1x'};
+                app.GridLayout.ColumnWidth = {220, '1x'};
+                app.RightPanel.Layout.Row = 1;
+                app.RightPanel.Layout.Column = 2;
+            end
+        end
+    end
+
+    % Component initialization
+    methods (Access = private)
+
+        % Create UIFigure and components
+        function createComponents(app)
+
+            % Create UIFigure and hide until all components are created
+            app.UIFigure = uifigure('Visible', 'off');
+            app.UIFigure.AutoResizeChildren = 'off';
+            app.UIFigure.Position = [100 100 640 480];
+            app.UIFigure.Name = 'MATLAB App';
+            app.UIFigure.SizeChangedFcn = createCallbackFcn(app, @updateAppLayout, true);
+
+            % Create GridLayout
+            app.GridLayout = uigridlayout(app.UIFigure);
+            app.GridLayout.ColumnWidth = {220, '1x'};
+            app.GridLayout.RowHeight = {'1x'};
+            app.GridLayout.ColumnSpacing = 0;
+            app.GridLayout.RowSpacing = 0;
+            app.GridLayout.Padding = [0 0 0 0];
+            app.GridLayout.Scrollable = 'on';
+
+            % Create LeftPanel
+            app.LeftPanel = uipanel(app.GridLayout);
+            app.LeftPanel.Layout.Row = 1;
+            app.LeftPanel.Layout.Column = 1;
+
+            % Create GridLayout2
+            app.GridLayout2 = uigridlayout(app.LeftPanel);
+            app.GridLayout2.RowHeight = {'1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x'};
+
+            % Create ComponentSelectionDropDownLabel
+            app.ComponentSelectionDropDownLabel = uilabel(app.GridLayout2);
+            app.ComponentSelectionDropDownLabel.HorizontalAlignment = 'right';
+            app.ComponentSelectionDropDownLabel.WordWrap = 'on';
+            app.ComponentSelectionDropDownLabel.Layout.Row = 2;
+            app.ComponentSelectionDropDownLabel.Layout.Column = 1;
+            app.ComponentSelectionDropDownLabel.Text = 'Component Selection';
+
+            % Create ComponentSelectionDropDown
+            app.ComponentSelectionDropDown = uidropdown(app.GridLayout2);
+            app.ComponentSelectionDropDown.Items = {'Tank,Engine,Point Mass,Option 4'};
+            app.ComponentSelectionDropDown.Layout.Row = 2;
+            app.ComponentSelectionDropDown.Layout.Column = 2;
+            app.ComponentSelectionDropDown.Value = 'Tank,Engine,Point Mass,Option 4';
+
+            % Create RightPanel
+            app.RightPanel = uipanel(app.GridLayout);
+            app.RightPanel.Layout.Row = 1;
+            app.RightPanel.Layout.Column = 2;
+
+            % Create UIAxes
+            app.UIAxes = uiaxes(app.RightPanel);
+            title(app.UIAxes, 'Rocket Layout', 'Interpreter', 'latex')
+            xlabel(app.UIAxes, 'X', 'Interpreter', 'latex')
+            ylabel(app.UIAxes, 'Y', 'Interpreter', 'latex')
+            zlabel(app.UIAxes, 'Z', 'Interpreter', 'latex')
+            app.UIAxes.Position = [12 267 396 199];
+
+            % Show the figure after all components are created
+            app.UIFigure.Visible = 'on';
+        end
+    end
+
+    % App creation and deletion
+    methods (Access = public)
+
+        % Construct app
+        function app = RocketGUI_exported
+
+            % Create UIFigure and components
+            createComponents(app)
+
+            % Register the app with App Designer
+            registerApp(app, app.UIFigure)
+
+            if nargout == 0
+                clear app
+            end
+        end
+
+        % Code that executes before app deletion
+        function delete(app)
+
+            % Delete UIFigure when app is deleted
+            delete(app.UIFigure)
+        end
+    end
+end
